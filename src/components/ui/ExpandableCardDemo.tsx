e007b048949e4af80992c298b14aaf11e95c9fd9
Gustavo Gantois <gugagantois@gmail.com>
2025-03-03 20:47:44 -0300

    feat:Adding projects in show cases section.

--- src/components/ui/ExpandableCardDemo.tsx
+++ src/components/ui/ExpandableCardDemo.tsx
@@ -68,7 +68,7 @@ export function ExpandableCardDemo() {
             <motion.div
               layoutId={`card-${active.title}-${id}`}
               ref={ref}
-              className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+              className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-neutral-900 sm:rounded-3xl overflow-hidden"
             >
               <motion.div layoutId={`image-${active.title}-${id}`}>
                 <Image
@@ -86,13 +86,13 @@ export function ExpandableCardDemo() {
                   <div className="">
                     <motion.h3
                       layoutId={`title-${active.title}-${id}`}
-                      className="font-bold text-neutral-700 dark:text-neutral-200"
+                      className="font-bold text-neutral-200"
                     >
                       {active.title}
                     </motion.h3>
                     <motion.p
                       layoutId={`description-${active.description}-${id}`}
-                      className="text-neutral-600 dark:text-neutral-400"
+                      className="text-neutral-400"
                     >
                       {active.description}
                     </motion.p>
@@ -102,7 +102,7 @@ export function ExpandableCardDemo() {
                     layoutId={`button-${active.title}-${id}`}
                     href={active.ctaLink}
                     target="_blank"
-                    className="px-4 py-3 text-sm rounded-full font-bold bg-green-500 text-white"
+                    className="px-4 py-3 text-sm rounded-full font-bold bg-[#0AE7E7] text-white"
                   >
                     {active.ctaText}
                   </motion.a>
@@ -113,7 +113,7 @@ export function ExpandableCardDemo() {
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
-                    className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
+                    className="text-neutral-400 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
                   >
                     {typeof active.content === "function"
                       ? active.content()
@@ -207,111 +207,169 @@ export const CloseIcon = () => {
 const cards = [
   {
     description: "Rafael Perrone",
-    title: "Landing Page",
+    title: "Landing Page | Nutrição",
     src: "https://assets.aceternity.com/demos/lana-del-rey.jpeg",
     ctaText: "Play",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "https://website-nutrition-rafael-perrone.vercel.app/",
     content: () => {
       return (
         <p>
-          Lana Del Rey, an iconic American singer-songwriter, is celebrated for
-          her melancholic and cinematic music style. Born Elizabeth Woolridge
-          Grant in New York City, she has captivated audiences worldwide with
-          her haunting voice and introspective lyrics. <br /> <br /> Her songs
-          often explore themes of tragic romance, glamour, and melancholia,
-          drawing inspiration from both contemporary and vintage pop culture.
-          With a career that has seen numerous critically acclaimed albums, Lana
-          Del Rey has established herself as a unique and influential figure in
-          the music industry, earning a dedicated fan base and numerous
-          accolades.
+          Projeto desenvolvido para o nutricionista Rafael Perrone: uma landing page estratégica para promover seus serviços com excelência e profissionalismo.
+          Este projeto foi criado com o objetivo de destacar a expertise do nutricionista Rafael Perrone e apresentar seus serviços de forma clara e atrativa. A landing page foi cuidadosamente planejada para transmitir confiança, profissionalismo e resultados eficazes, oferecendo aos visitantes uma experiência 
+          única ao conhecerem os serviços especializados de Rafael.
         </p>
       );
     },
   },
   {
-    description: "Babbu Maan",
-    title: "Mitran Di Chhatri",
+    description: "Novasce",
+    title: "Landing Page | Empresa de software",
     src: "https://assets.aceternity.com/demos/babbu-maan.jpeg",
     ctaText: "Play",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "https://novasce-website-next-tailwind-css.vercel.app/",
     content: () => {
       return (
         <p>
-          Babu Maan, a legendary Punjabi singer, is renowned for his soulful
-          voice and profound lyrics that resonate deeply with his audience. Born
-          in the village of Khant Maanpur in Punjab, India, he has become a
-          cultural icon in the Punjabi music industry. <br /> <br /> His songs
-          often reflect the struggles and triumphs of everyday life, capturing
-          the essence of Punjabi culture and traditions. With a career spanning
-          over two decades, Babu Maan has released numerous hit albums and
-          singles that have garnered him a massive fan following both in India
-          and abroad.
+          Landing page desenvolvida para a Novasce, empresa especializada em terceirização de software.
+          A Novasce solicitou o desenvolvimento de uma landing page 
+          com o objetivo de destacar sua expertise e profissionalismo 
+          no setor de terceirização de software. Este projeto foi 
+          cuidadosamente alinhado à identidade visual da empresa, 
+          garantindo que o design refletisse 
+          sua marca e valores de forma consistente e impactante.
         </p>
       );
     },
   },
 
   {
-    description: "Metallica",
-    title: "For Whom The Bell Tolls",
+    description: "SF Team",
+    title: "Landing Page | Consultoria de Bodybuilding",
     src: "https://assets.aceternity.com/demos/metallica.jpeg",
     ctaText: "Play",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "https://lp-sf-team-consultoria.vercel.app/",
     content: () => {
       return (
         <p>
-          Metallica, an iconic American heavy metal band, is renowned for their
-          powerful sound and intense performances that resonate deeply with
-          their audience. Formed in Los Angeles, California, they have become a
-          cultural icon in the heavy metal music industry. <br /> <br /> Their
-          songs often reflect themes of aggression, social issues, and personal
-          struggles, capturing the essence of the heavy metal genre. With a
-          career spanning over four decades, Metallica has released numerous hit
-          albums and singles that have garnered them a massive fan following
-          both in the United States and abroad.
+          Este projeto foi criado com o objetivo de destacar a expertise de 
+          Samuel Fernandes no universo do bodybuilding, oferecendo uma experiência visual 
+          e funcional que transmite sua dedicação, paixão e compromisso com o sucesso de 
+          seus alunos. A landing page foi projetada para refletir a garra e a essência que 
+          definem a abordagem única de Samuel, com foco em resultados reais e transformadores.
         </p>
       );
     },
   },
   {
-    description: "Led Zeppelin",
-    title: "Stairway To Heaven",
+    description: "MS Sampaio",
+    title: "Landing page | Advocacía",
     src: "https://assets.aceternity.com/demos/led-zeppelin.jpeg",
     ctaText: "Play",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "https://msampaio-adv.vercel.app/",
     content: () => {
       return (
         <p>
-          Led Zeppelin, a legendary British rock band, is renowned for their
-          innovative sound and profound impact on the music industry. Formed in
-          London in 1968, they have become a cultural icon in the rock music
-          world. <br /> <br /> Their songs often reflect a blend of blues, hard
-          rock, and folk music, capturing the essence of the 1970s rock era.
-          With a career spanning over a decade, Led Zeppelin has released
-          numerous hit albums and singles that have garnered them a massive fan
-          following both in the United Kingdom and abroad.
+         Esta landing page foi criada com o objetivo de refletir o 
+         compromisso e a dedicação de Maurício Sampaio com cada 
+         cliente e caso que ele assume. Através de um design 
+         elegante e funcional, a página transmite a seriedade e
+          a expertise do advogado, enfatizando sua abordagem 
+          personalizada e atenciosa 
+         para solucionar as necessidades jurídicas de seus clientes.
         </p>
       );
     },
   },
   {
-    description: "Mustafa Zahid",
-    title: "Toh Phir Aao",
+    description: "Mônica Aran",
+    title: "Landing Page | Psicologia",
     src: "https://assets.aceternity.com/demos/toh-phir-aao.jpeg",
     ctaText: "Play",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "https://www.psimonicaaran.com.br/",
     content: () => {
       return (
         <p>
-          &quot;Aawarapan&quot;, a Bollywood movie starring Emraan Hashmi, is
-          renowned for its intense storyline and powerful performances. Directed
-          by Mohit Suri, the film has become a significant work in the Indian
-          film industry. <br /> <br /> The movie explores themes of love,
-          redemption, and sacrifice, capturing the essence of human emotions and
-          relationships. With a gripping narrative and memorable music,
-          &quot;Aawarapan&quot; has garnered a massive fan following both in
-          India and abroad, solidifying Emraan Hashmi&apos;s status as a
-          versatile actor.
+          Esta landing page foi cuidadosamente elaborada para destacar o trabalho 
+          excepcional de Mônica Aran, psicóloga especializada em gestalt-terapia. 
+          A página foi projetada para transmitir a empatia, o carinho e a dedicação
+           com que Mônica atende cada um de seus pacientes, refletindo 
+          seu compromisso em oferecer um acompanhamento terapêutico humanizado e eficaz.
+        </p>
+      );
+    },
+  },
+  {
+    description: "Full Dev Community",
+    title: "Saas | Sistema de gerênciamento de atividades diárias/semanais.",
+    src: "https://assets.aceternity.com/demos/toh-phir-aao.jpeg",
+    ctaText: "Play",
+    ctaLink: "https://fulldev-seven.vercel.app/?fbclid=PAZXh0bgNhZW0CMTEAAaZTMuQeNDinOGKcBt59yV2zoTk5G0r3EB5lYZB2tXQvOoB4sH5JM1rn0oo_aem__QaWJekN-srZCm1KDCDSzQ",
+    content: () => {
+      return (
+        <p>
+         Este sistema foi criado com foco no gerenciamento de tempo, permitindo 
+         aos usuários organizar suas atividades por dias, semanas e meses de 
+         forma eficiente. Utilizando uma stack robusta, o projeto foi desenvolvido
+          com Next.js, TypeScript, TailwindCSS, Java e banco de dados Prisma. A solução
+           oferece uma interface intuitiva e recursos avançados para otimizar a gestão de
+            tempo, sendo uma ferramenta valiosa para profissionais e equipes que buscam 
+         aumentar sua produtividade e manter o controle de suas tarefas.
+        </p>
+      );
+    },
+  },
+  {
+    description: "Clínica Hayashi",
+    title: "Landing Page | Serviços Médicos.",
+    src: "https://assets.aceternity.com/demos/toh-phir-aao.jpeg",
+    ctaText: "Play",
+    ctaLink: "https://clinicahayashi.app.br/",
+    content: () => {
+      return (
+        <p>
+        A landing page foi cuidadosamente projetada para refletir a dedicação, experiência 
+        e excelência do Dr. Matheus Hayashi nas áreas em que é especializado. O design busca
+         transmitir a sua paixão pela medicina, seu compromisso com a saúde dos pacientes e 
+         seu estilo de vida profissional. Com um layout moderno e informativo, a página 
+         apresenta de forma clara as qualificações do Dr. 
+        Matheus, os serviços oferecidos e sua visão inovadora dentro da área médica.
+        </p>
+      );
+    },
+  },
+  {
+    description: "Space Blog",
+    title: "Blog | Blog desenvolvido para abordar assuntos e temas espaciais.",
+    src: "https://assets.aceternity.com/demos/toh-phir-aao.jpeg",
+    ctaText: "Play",
+    ctaLink: "https://space-blog-mu.vercel.app/",
+    content: () => {
+      return (
+        <p>
+        Esta landing page foi projetada para proporcionar uma imersão completa no universo espacial, 
+        utilizando elementos visuais e interativos que capturam a essência desse tema. 
+        O design da interface (UI) foi cuidadosamente elaborado para oferecer uma experiência única e envolvente,
+         transportando o usuário para um ambiente dinâmico e instigante, com foco em usabilidade e estética. Cada 
+         detalhe foi pensado para 
+        criar uma experiência visual que mantenha o usuário imerso e interessado, promovendo uma navegação fluída e interativa.
+        </p>
+      );
+    },
+  },
+  {
+    description: "Dev Streak",
+    title: "Landing Page | Captação de leads para aplicativo.",
+    src: "https://assets.aceternity.com/demos/toh-phir-aao.jpeg",
+    ctaText: "Play",
+    ctaLink: "https://fulldev-seven.vercel.app/?fbclid=PAZXh0bgNhZW0CMTEAAaZTMuQeNDinOGKcBt59yV2zoTk5G0r3EB5lYZB2tXQvOoB4sH5JM1rn0oo_aem__QaWJekN-srZCm1KDCDSzQ",
+    content: () => {
+      return (
+        <p>
+         O aplicativo, conforme descrito pelo seu criador, visa otimizar o gerenciamento de tempo e ajudar o 
+         usuário a manter o foco de maneira semelhante ao sistema do Duolingo. A proposta é criar uma experiência
+          envolvente e eficaz, que incentive os usuários a atingirem seus objetivos de forma contínua e disciplinada.
+           A landing page foi projetada para atrair e converter leads, destacando os principais 
+         benefícios do aplicativo e incentivando os visitantes a se cadastrarem para experimentar a ferramenta.
         </p>
       );
     },
