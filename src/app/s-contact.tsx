cfc62b04c637246ba72245741871f0930ab4eca0
Gustavo Gantois <gugagantois@gmail.com>
2025-03-03 19:00:55 -0300

    feat:Creating contact section and adding circular image.

--- src/app/s-contact.tsx
+++ src/app/s-contact.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export function ContactSection() {
     return (
-        <section className="mt-20 lg:mt-40">
-            <div className="flex flex-col justify-center items-center gap-4">
-                <h1 className="flex flex-col justify-center items-center text-4xl text-white">
+        <section id="contact" className="mt-40 lg:mt-60 relative z-50">
+            <div className="flex flex-col justify-center items-center gap-4 z-50">
+                <h1 className="flex flex-col justify-center text-center items-center text-4xl text-white">
                     Entre em contato
                     <span className="text-[#00FFFF]">Profissionalismo e Soluções Garantidas!</span>
                 </h1>
@@ -13,11 +14,16 @@ export function ContactSection() {
                 Vamos transformar sua ideia em realidade! Entre em contato e conte com um serviço profissional, 
                 ágil e 100% focado na melhor solução para você.
                 </p>
-                <Button className="p-6" variant="secondary" asChild>
+                <Button className="p-6 z-50" variant="secondary" asChild>
                     <Link href="" target="_blank">
                         Vamos trabalhar juntos!
                     </Link>
                 </Button>
+            </div>
+            <div className="flex items-center justify-center z-10">
+            <div className="absolute -top-20 left-0 right-0">
+                <Image className="w-full -z-50" src="/CircleImgContact.svg" width={1400} height={200} alt="" />
+            </div>
             </div>
         </section>
     )
